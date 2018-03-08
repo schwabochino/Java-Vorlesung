87fefd1d00181418ed9f82a67ee923b0bcb90ffa
schwabochino <philipp.schwarberg@outlook.de>
2018-03-08 20:23:54 +0100

    Quadrate und Brutto Netto
    Übung zum warm werden mit Java ;)

--- 01/src/main/java/Squared.java
+++ 01/src/main/java/Squared.java
@@ -2,7 +2,7 @@
 public class Squared {
 
 	public static void main(String[] args) {
-		int n = 2;
+		int n = 12;
 		System.out.println("Quadrat: " + n * n);
 
 	}
---
+++ Mwst-Rechner/src/main/java/Mwst.java
@@ -0,0 +1,14 @@
+
+public class Mwst {
+
+	public static void main(String[] args) {
+		double mwst = 0.19;
+		double betrag = 100;
+		
+		System.out.printf("Netto %.2f %n", + betrag);
+		System.out.printf("MWST: %.2f %n", + mwst * betrag);
+		System.out.printf("Brutto: %.2f %n", + ((betrag*mwst) + betrag));
+
+	}
+
+}
