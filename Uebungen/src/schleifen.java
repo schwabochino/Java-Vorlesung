63004448578843c543ecb7973d24db14b179c160
schwabochino <philipp.schwarberg@outlook.de>
2018-03-10 21:04:31 +0100

    Kommentare

--- Uebungen/src/schleifen.java
+++ Uebungen/src/schleifen.java
@@ -5,13 +5,13 @@ public class schleifen
 	public static void main(String[] args) 
 	{
 		int y=0 , i=0;
-		for (i=0; i<=9; i++)
+		for (i=0; i<=9; i++) //Schleife zählt bis 9 hoch
 		{
-			for (y=0; y<i; y++)
+			for (y=0; y<i; y++) //vergleicht i und y und gibt dann solange i aus bis y>i
 			{
 				System.out.printf("%d",+ i);	
 			}
-			System.out.printf("\n");							
+			System.out.printf("\n");							//Zeilenumbruch
 		}
 		
 
