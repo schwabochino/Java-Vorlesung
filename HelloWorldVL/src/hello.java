0b1a5b7faa18f3f4206a1507bb0df3ea9bd7e6af
Philipp Schwarberg <philipp.schwarberg@outlook.de>
2018-03-10 10:38:07 +0100

    Vorlesungszeugs

--- HelloWorldVL/src/hello.java
+++ HelloWorldVL/src/hello.java
@@ -1,9 +1,9 @@
 
 public class hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+	public static void main(String[] args) { //Methode Main
+		// TODO Auto-generated method stub  
+		System.out.println("Hello World!"); //Ausgabe Hello World!
 
 	}
 
---
+++ Kuh/src/main.java
@@ -0,0 +1,33 @@
+class Kuh
+{
+	char name;// = Elsa;
+	char nachname;// = Euter;
+	int geburtsjahr; // = 1998;
+	float gewicht;
+	
+	char get_name()
+	{
+		return name;
+	}
+	char get_nachname()
+	{
+		return nachname;
+	}
+	int get_geburtsjahr()
+	{
+		return geburtsjahr;
+	}
+	float get_gewicht()
+	{
+		return gewicht;
+	}
+}
+public class main {
+
+	public static void main(String[] args) {
+		Kuh k1 = new Kuh();
+		System.out.println(k1.get_name());
+		
+	}
+
+}
